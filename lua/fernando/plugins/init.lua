46f2cefdb5a2d56b0312c7f1eb34db4e5e0a2c77
FernandoPizzardo <fermelo95@gmail.com>
2024-07-01 11:28:45 -0300

    git repos + flutter when mac

--- lua/fernando/plugins/init.lua
+++ lua/fernando/plugins/init.lua
@@ -1,1 +1,11 @@
-return { 'tpope/vim-fugitive' }
+if (vim.fn.has('macunix')) then
+  return {
+        'akinsho/flutter-tools.nvim',
+        lazy = true,
+        dependencies = {
+          'nvim-lua/plenary.nvim',
+          'stevearc/dressing.nvim', -- optional for vim.ui.select
+        },
+        config = true,
+      }
+end
