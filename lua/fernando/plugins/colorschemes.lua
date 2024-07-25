cf3f98570cdc9a306da89cfb1c508154b7ad2d75
FernandoPizzardo <fermelo95@gmail.com>
2024-07-25 12:26:46 -0300

    yazi support

--- lua/fernando/plugins/colorschemes.lua
+++ lua/fernando/plugins/colorschemes.lua
@@ -1,4 +1,10 @@
 return {
-{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = { transparent_background = true, }
+  },
+
 
 }
