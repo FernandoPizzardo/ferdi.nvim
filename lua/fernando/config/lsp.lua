ec0207ce2d981d78167625791da4147dab5c0165
Fernando Melo Pizzardo <fpizzardo.brq@bancomaster.com.br>
2024-10-20 10:55:54 -0300

    copilot aeho

--- lua/fernando/config/lsp.lua
+++ lua/fernando/config/lsp.lua
@@ -6,6 +6,7 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 require("mason").setup()
+require("flutter-tools").setup { fvm = true }
 require("mason-lspconfig")
 require("mason-lspconfig").setup_handlers {
   function(server_name)
@@ -14,8 +15,50 @@ require("mason-lspconfig").setup_handlers {
 }
 local cmp = require('cmp')
 
+local lspkind = require('lspkind')
+lspkind.init({
+  symbol_map = {
+    Copilot = "",
+  },
+})
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+
+vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-  }),
+
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      -- can also be a function to dynamically calculate max width such as
+      -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+      ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+    })
+  },
+  sources = {
+    -- Copilot Source
+    { name = "copilot",  group_index = 2 },
+    -- Other Sources
+    { name = "nvim_lsp", group_index = 2 },
+    { name = "path",     group_index = 2 },
+  },
+  mapping = {
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+    ["<Tab>"] = vim.schedule_wrap(function(fallback)
+      if cmp.visible() and has_words_before() then
+        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+      else
+        fallback()
+      end
+    end),
+  }
 })
--- lua/fernando/plugins/init.lua
+++ lua/fernando/plugins/init.lua
@@ -1,11 +1,9 @@
-if (vim.fn.has('macunix')) then
-  return {
-        'akinsho/flutter-tools.nvim',
-        lazy = true,
-        dependencies = {
-          'nvim-lua/plenary.nvim',
-          'stevearc/dressing.nvim', -- optional for vim.ui.select
-        },
-        config = true,
-      }
-end
+return {
+  'akinsho/flutter-tools.nvim',
+  lazy = false,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'stevearc/dressing.nvim', -- optional for vim.ui.select
+  },
+  config = true,
+}
